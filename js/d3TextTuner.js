6fc5328da6680a35521fe10c9757c4c8dc4070a0
Slava <trlearning@mail.ru>
2016-05-07 09:35:33 +0300

    add new colors for different texts

--- js/d3TextTuner.js
+++ js/d3TextTuner.js
@@ -1,3 +1,6 @@
 $(function(){
+  var remTextColor = '#f90123';
+  var siTextColor = '#01cba5';
   d3.select('.d3rem').style('color', '#627a81');
+  d3.select('.si').style('color', siTextColor);
 });
