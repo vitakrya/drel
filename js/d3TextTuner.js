4bb7149297a1d11bd2b52f0b47aed7e7240fc000
Slava <trlearning@mail.ru>
2016-05-07 11:04:10 +0300

    add third, fourth colors
    add fifth, sixth colors
    
    will it be last commit message

--- js/d3TextTuner.js
+++ js/d3TextTuner.js
@@ -1,6 +1,14 @@
 $(function(){
   var remTextColor = '#f90123';
   var siTextColor = '#01cba5';
+  var thirdColor = '#ee1121';
+  var fourthColor = '#01b2c2';
+  var fifthColor = '#01b013';
+  var sixthColor = '#09dbca';
+  var seventhColor = '#d701ba';
+  var eighthColor = '#ee1829';
+  var ninethColor = '#a0b0c9';
+
   d3.select('.d3rem').style('color', '#627a81');
   d3.select('.si').style('color', siTextColor);
 });
